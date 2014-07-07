7bdcf9082f1e6674946da7d876e34bbd3052f029
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-07 16:11:08 +0800

    Added create-command-line-interface.js file.

---
+++ create-command-line-interface.js
@@ -0,0 +1,24 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "spivak",
+			"fileName": "parse-command.js",
+			"moduleName": "parseCommand",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/spivak.git"
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		
+	@end-module-documentation
+
+	@include:
+	@end-include
+*/
+var createCommandLineInterface = function createCommandLineInterface( promptString, interfaceHandler ){
+
+};
+
+( module || { } ).exports = createCommandLineInterface;
